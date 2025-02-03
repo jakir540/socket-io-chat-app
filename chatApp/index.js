3cb980ebf1ff48007da8d51cbeeb8c1fc70f3998
jakir540 <jakirhossainmd540@gamil.com>
2025-02-03 23:59:43 +0600

    add api

--- chatApp/index.js
+++ chatApp/index.js
@@ -6,6 +6,23 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.get("/", (req, res) => {
+  res.send("Socket.IO Server is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (msg) => {
+    console.log("Message received: ", msg);
+    io.emit("message", msg); // Broadcast the message to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
